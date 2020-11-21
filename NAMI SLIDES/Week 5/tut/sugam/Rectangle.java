323aade8f4c0b03cf4d1bcf46284b79d47244b8b
magusikrak <sugamkarki7058@gmail.com>
2020-11-21 22:52:40 +0545

    sugam

---
+++ NAMI SLIDES/Week 5/tut/sugam/Rectangle.java
@@ -0,0 +1,28 @@
+package sugam;
+
+class Rectangle {
+    int width, height;
+    String str;
+
+    // public Rectangle(int width, int height, String str) {
+    //     this.width = width;
+    //     this.height = height;
+    //     this.str = str;
+    // }
+
+    // public void draw() {
+    //     for (int i = 0; i < this.height; i++) {
+    //         if (i == 0) {
+    //             System.out.print("+-----+\n");
+    //         }
+    //         System.out.print("|");
+    //         for (int j = 0; j < this.width; j++) {
+    //             System.out.print(str);
+    //         }
+    //         System.out.print("|\n");
+
+    //         System.out.print("+-----+\n");
+    //     }
+    // }
+
+}
---
+++ NAMI SLIDES/Week 5/tut/sugam/caller.java
@@ -0,0 +1,11 @@
+/**
+ * Rectangle
+ */
+package sugam;
+
+public class caller {
+    public static void main(String[] args) {
+        Rectangle rect=new Rectangle(5,4,"@");
+        rect.draw();
+    }
+}
