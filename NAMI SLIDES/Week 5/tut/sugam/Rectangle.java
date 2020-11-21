1181c0acef77b7fc45319f5cd6b3e231ca82a09b
magusikrak <sugamkarki7058@gmail.com>
2020-11-21 22:56:06 +0545

    o

--- NAMI SLIDES/Week 5/tut/sugam/Rectangle.java
+++ NAMI SLIDES/Week 5/tut/sugam/Rectangle.java
@@ -4,25 +4,25 @@ class Rectangle {
     int width, height;
     String str;
 
-    // public Rectangle(int width, int height, String str) {
-    //     this.width = width;
-    //     this.height = height;
-    //     this.str = str;
-    // }
+    public Rectangle(int width, int height, String str) {
+        this.width = width;
+        this.height = height;
+        this.str = str;
+    }
 
-    // public void draw() {
-    //     for (int i = 0; i < this.height; i++) {
-    //         if (i == 0) {
-    //             System.out.print("+-----+\n");
-    //         }
-    //         System.out.print("|");
-    //         for (int j = 0; j < this.width; j++) {
-    //             System.out.print(str);
-    //         }
-    //         System.out.print("|\n");
+    public void draw() {
+        for (int i = 0; i < this.height; i++) {
+            if (i == 0) {
+                System.out.print("+-----+\n");
+            }
+            System.out.print("|");
+            for (int j = 0; j < this.width; j++) {
+                System.out.print(str);
+            }
+            System.out.print("|\n");
 
-    //         System.out.print("+-----+\n");
-    //     }
-    // }
+            System.out.print("+-----+\n");
+        }
+    }
 
 }
